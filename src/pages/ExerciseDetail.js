86de178cc13518c046ec1e77ede8f8af1114e9f9
Ramish Hassan <ramishshah93@gmail.com>
2022-08-17 11:17:23 +0200

    componenet Details.js complete, showing gifs and relevent info about exercise

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -8,9 +8,29 @@ import Details from "../components/Details";
 import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
+        exerciseOptions
+      );
+      console.log({ exerciseDetailData });
+      setExerciseDetail(exerciseDetailData);
+    };
+
+    fetchExerciseData();
+  }, [id]);
+
   return (
     <Box>
-      <Details />
+      <Details exerciseDetail={exerciseDetail} />
       <ExerciseVideos />
       <SimilarExercises />
     </Box>
